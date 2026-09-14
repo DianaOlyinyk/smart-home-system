29194177d10846f2388037d68bdb4dd8e454edcf
Oleg Moshenskiy <o.moshenskyi@ukma.edu.ua>
2026-09-14 18:40:53 +0300

    Add DeviceAccess entity with relevant fields

--- common/src/main/java/DeviceAccess.java
+++ common/src/main/java/DeviceAccess.java
@@ -1,1 +1,15 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.Instant;
+import java.util.UUID;
 
+@Entity
+public class DeviceAccess {
+    @Id
+    UUID id;
+    UUID deviceId;
+    UUID userId;
+    String role;
+    UUID grantedById;
+    Instant grantedAt;
+}
